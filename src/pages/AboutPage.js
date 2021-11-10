85b8d683c35516aef91440636b5e67b7445543da
Daniel Rabinkov <rabinkovdanny@gmail.com>
2021-11-10 21:27:23 +0200

    style about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,12 +3,22 @@ import { GiDiamondsSmile } from "react-icons/gi";
 
 function AboutPage() {
   return (
-    <Container>
-      <h1 style={{ fontFamily: "ariel", fontSize: "2rem", padding: "20px" }}>
+    <Container className="mainCont">
+      <h1>
         {" "}
         My First React Project &nbsp; <GiDiamondsSmile></GiDiamondsSmile>
       </h1>
-      <p className="aboutPara" style={{ fontFamily: "ariel" }}>
+      <p
+        className="aboutPara"
+        style={{
+          fontFamily: "ariel",
+          boxShadow:
+            "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset",
+          marginTop: "4rem",
+          padding: "3rem",
+          backgroundColor: "darkgray",
+        }}
+      >
         This is my first react project. In this site you can register to you'r
         own user and "collect" you'r favoarit stores that you'r usually buying
         from.Also, you can register as a business to open you'r own cards THAT
